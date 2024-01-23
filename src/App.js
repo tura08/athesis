838b7dd523d51f427a5ba6bf833bcdca071eb686
Andrea Turazza <andreaturazza94@gmail.com>
2024-01-23 14:58:16 +0100

    Using HashRouter for dealing with redirect issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navigation from "./pages/navigation/Navigation";
 import Home from "./pages/home/Home";
